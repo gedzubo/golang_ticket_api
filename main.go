043347c95e8e0a26fb448ab7845e2dc4edba0b94
Gediminas Z <gediminas.zubovicius@gmail.com>
2024-11-26 17:08:34 +0000

    Adding a new endpoint to create ticket options

--- main.go
+++ main.go
@@ -27,12 +27,24 @@ func getTicketOption(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, ticketOption)
 }
 
+func createTicketOption(ctx *gin.Context) {
+	var ticketOption models.TicketOption
+
+	if err := ctx.ShouldBindJSON(&ticketOption); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, ticketOption)
+}
+
 func main() {
 	models.ConnectToDatabase()
 
 	r := gin.Default()
 
 	r.GET("/ticket_options/:id", getTicketOption)
+	r.POST("/ticket_options", createTicketOption)
 
 	r.Run("localhost:3000")
 }
